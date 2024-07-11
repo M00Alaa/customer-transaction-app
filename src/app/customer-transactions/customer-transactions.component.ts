e7e9b915f5aaaa349823f5fe85fc286d7b223281
M00Alaa <mohamedalaa.ng@gmail.com>
2024-07-11 12:36:52 +0300

    finish customer transactions page

--- src/app/customer-transactions/customer-transactions.component.ts
+++ src/app/customer-transactions/customer-transactions.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrl: './customer-transactions.component.scss'
 })
 export class CustomerTransactionsComponent {
+  searchText = '';
+  searchAmount: number | undefined = undefined;
+  gettingTransactions = false;
 
+  customers = [
+    { id: 1, name: "Ahmed Ali" },
+    { id: 2, name: "Aya Elsayed" },
+    { id: 3, name: "Mina Adel" },
+    { id: 4, name: "Sarah Reda" },
+    { id: 5, name: "Mohamed Sayed" }
+  ];
+
+  transactions = [
+    { id: 1, customer_id: 1, date: "2022-01-01", amount: 1000 },
+    { id: 2, customer_id: 1, date: "2022-01-02", amount: 2000 },
+    { id: 3, customer_id: 2, date: "2022-01-01", amount: 550 },
+    { id: 4, customer_id: 3, date: "2022-01-01", amount: 500 },
+    { id: 5, customer_id: 2, date: "2022-01-02", amount: 1300 },
+    { id: 6, customer_id: 4, date: "2022-01-01", amount: 750 },
+    { id: 7, customer_id: 3, date: "2022-01-02", amount: 1250 },
+    { id: 8, customer_id: 5, date: "2022-01-01", amount: 2500 },
+    { id: 9, customer_id: 5, date: "2022-01-02", amount: 875 }
+  ];
+
+  filteredTransactions = [...this.transactions];
+
+  ngOnInit(): void {
+    this.gettingTransactions = true;
+    setTimeout(() => {
+      this.gettingTransactions = false;
+    }, 1000);
+  }
+
+  getCustomerName(id: number) {
+    return this.customers.find(c => c.id === id)?.name;
+  }
+
+  getAllTransactions() {
+    this.filteredTransactions = this.transactions.filter(transaction => {
+      const customerName = this.getCustomerName(transaction.customer_id)?.toLowerCase() || '';
+      const matchesName = customerName.includes(this.searchText.toLowerCase());
+      const matchesAmount = this.searchAmount ? transaction.amount === +this.searchAmount : true;
+      // const matchesAmount = transaction.amount.toString().includes(this.searchAmount!.toString()) || '';
+      return matchesName && matchesAmount;
+    });
+  }
 }
